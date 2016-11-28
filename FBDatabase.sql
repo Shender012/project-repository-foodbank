4d47ab3e36535c62683dec2a2d1d3076048e91ed
Cindy Howard <howardcy@lewisu.edu>
2016-11-28 12:48:31 -0600

    updates to sql script

--- FBDatabase.sql
+++ FBDatabase.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE ni_monitoring; 
+CREATE DATABASE ni_monitoring;
 
 CREATE TABLE fb_agency (
 	/*Network Partner Monitoring Form */
-    P_id int NOT NULL, 
-    ADD PRIMARY KEY (P_id), 
+    P_id int NOT NULL,
+    ADD PRIMARY KEY (P_id),
     AgencyCountry varchar(255),
     AgencyNumber int,
     TodaysDate date,
     DateOfLastMonitor date,
-    AgencyName varchar(255), 
+    AgencyName varchar(255),
     AgencyNumber int,
     ParentAgencyName varchar(255),
     ParentAgencyNumber int,
@@ -25,6 +25,8 @@ CREATE TABLE fb_agency (
     DateOfIRSVarification date,
     EmergencyFoodProgram boolean,
     DateOFContract date,
+
+    /*not sure we need these booleans - use fb_agency PK as FK to link */
     MP_SoupKitchen boolean,
     MP_GroupHome boolean,
     MP_Shelter_TransitionalLiving boolean,
@@ -37,45 +39,50 @@ CREATE TABLE fb_agency (
  );
 
 CREATE TABLE fb_interview (
-    Intrw_id int, 
+    Agency_id int, /*this links the interview to a particular agency*/
+    Intrw_id int,
     PersonInterview varchar(255),
     DaysOfOperation varchar(255), /*Need to know if "days' (varchar) or dates (date) */
     TimeOfOperationStart time,
-    TimeOfOperationStop time, 
-    IsYourOrgRequiredToHaveASingleAudit boolean, 
+    TimeOfOperationStop time,
+    IsYourOrgRequiredToHaveASingleAudit boolean,
     DoesTheAgencyAppearToBeHandicappedAccessible boolean,
 
     /*Part III: Food Safety, Storage and Inventory*/
-    DoesTheLocalHealthDepartmentRequireAPermit boolean, 
-    IfYesProvideDateOfLastInspection date, 
+    DoesTheLocalHealthDepartmentRequireAPermit boolean,
+    IfYesProvideDateOfLastInspection date,
     IfYesProvideCopyOfPermitOfLastInspectionScore int, //What to do fro conditional variables
     IsThereACleaningScheduleEstablishedAndPosted boolean,
     PestControlRoutine boolean,
     PestControlCompany varchar(255),
-    PestControlLastVisit date, 
+    PestControlLastVisit date,
     IsAllFoodStoredAtLocation boolean,
     IfNoWhereIsFoodStored varchar(255), //What to do fro conditional variables
-    IsAllFoodStoredSixInchesOffFloor boolean, 
-    RefrigeratorTempBetween35and40F boolean, 
+    IsAllFoodStoredSixInchesOffFloor boolean,
+    RefrigeratorTempBetween35and40F boolean,
     RefrigeratorTemp int,
-    FreezerTemp0orBelow boolean, 
+    FreezerTemp0orBelow boolean,
     FreezerTemp int,
     WorkingThermometersInStorageAreas boolean,
-    ThermometerTempsInProperRange boolean, 
-    TempLogsMaintainedAndPosted boolean  
+    ThermometerTempsInProperRange boolean,
+    TempLogsMaintainedAndPosted boolean,
+    PRIMARY KEY(Agency_id, Intrw_id), /* composite primary key*/
+    CONSTRAINT fb_interview_fk_agency
+        FOREIGN KEY (Agency_id) REFERENCES fb_agency(P_id) /* this creates a FK constraint*/
 	//link optional parts
+
 );
 
-CREATE TABLE meal_program_information ( 
-    Intrw_id int, 
-    MonthlyServiceData boolean, 
-	NumberOfMealsServerdLastMonth int, 
-	DoesFoodBankHaveSafeHandlingCertification boolean, 
+CREATE TABLE meal_program_information (
+    Intrw_id int,
+    MonthlyServiceData boolean,
+	NumberOfMealsServerdLastMonth int,
+	DoesFoodBankHaveSafeHandlingCertification boolean,
 	SafeHandlingCertName varchar(255),
-	SafeHandlingCertDate date, 
-	FeesDonationsMembershipsRequired	boolean, 
-	FeesDonationMembershipsExplaination varchar(255), 
-	FundingSource varchar(255), 
+	SafeHandlingCertDate date,
+	FeesDonationsMembershipsRequired	boolean,
+	FeesDonationMembershipsExplaination varchar(255),
+	FundingSource varchar(255),
 	SafeHandlingCertPostedInKitchen boolean,
 	DaysOfOperationPostedVisible boolean,
 	RequirementsForFoodAssistance varchar(255),
@@ -83,34 +90,34 @@ CREATE TABLE meal_program_information (
 	GRPHOME_NumOfResidents int,
 	GRPHOME_NumOfMealsServed int,
 	GRPHOME_WhoParticipatedInMealPrograms varchar(255),
-	SCHL_NumOfFreeLunches int, 
-	SCHL_NumOfReducedLunches int, 
-	SCHL_LunchFee int, 
+	SCHL_NumOfFreeLunches int,
+	SCHL_NumOfReducedLunches int,
+	SCHL_LunchFee int,
 	SCHL_ReducedLunchFee int
 );
 CREATE TABLE meal_grocery_program_information (
-    Intrw_id int, 
+    Intrw_id int,
 	DistMthd_ChoicePantry boolean,
 	DistMthd_Traditional boolean,
 	DistMthd_Combination boolean,
 	DistMthd_Other boolean,
 	DistMthd_OtherMethod varchar(255),
 	HowOftenDoesHouseholdReceiveFood varchar(255),
-	AvgNumOfServedPerMonth int, 
-	AvgNumOfHouseHoldsServedPerMonth int, 
-	AvgAmntOfFoodPerHouseInLBORDays int, 
+	AvgNumOfServedPerMonth int,
+	AvgNumOfHouseHoldsServedPerMonth int,
+	AvgAmntOfFoodPerHouseInLBORDays int,
 	DesignatedGeographicalServiceArea boolean,
 	MemberAttendedFoodSafetyClass boolean,
 	MemberName varchar(255),
 	SafetyClassDate date,
 	FeesDonationsMembershipsRequired boolean,
-	FeesDonationMembershipsExplaination varchar(255), 
+	FeesDonationMembershipsExplaination varchar(255),
 	FundingSources varchar(255),
 	DaysOfOperationPostedVisible boolean,
 	RequirementsForFoodAssistance varchar(255),
 	ClientParticipationTracking boolean,
 	IntakeDoneInConfidential boolean,
-	FoodDeliveredToClients boolean	
+	FoodDeliveredToClients boolean
 );
 CREATE TABLE direct_connect_program (
     Intrw_id int,
@@ -120,11 +127,11 @@ CREATE TABLE direct_connect_program (
 	SampleTempsDocumentedAtTimeOfPickUpandDelivery boolean,
 	TempControlledDevicesForFoodTransport boolean
 );
-CREATE TABLE emergency_food_program_EEP_information ( 
+CREATE TABLE emergency_food_program_EEP_information (
     Intrw_id int,
 	DiscriminationComplaintsInPastYear boolean,
 	HaveComplaintsBeenForwardedToFoodBank boolean,
-	TotalCasesOfEFP_TANFProductInInventory int, 
+	TotalCasesOfEFP_TANFProductInInventory int,
 	DoesThePartnerOperateAnotherProgram boolean,
 	IsFoodSeperatedandTrackedForTheTwoProgram boolean,
 	DoesProgramHaveEFPSiteManualOnSite boolean,
@@ -133,7 +140,7 @@ CREATE TABLE emergency_food_program_EEP_information (
 	Religiousorganization boolean,
 	WrittenNoticeOfBeneficiaryRightPosterVisible boolean,
 	ClientsOfferedAReferralToANonReligousEFPFeedingProgram boolean,
-	/*Program Procedures*/ 
+	/*Program Procedures*/
 	DocumentsRequiredFromClient boolean,
 	TypesOfDocumentsRequested varchar(255),
 	PurposeForRequestingDocumentation varchar(255),
@@ -155,26 +162,26 @@ CREATE TABLE emergency_food_program_EEP_information (
 	ProxyFormContainsOriginalSignatureOfRecipient boolean,
 	DesignatedProxySignTheProxyFormInPresebceOfPantryPersonnel boolean
 );
-CREATE TABLE forms ( 
+CREATE TABLE forms (
     Intrw_id int,
 	AgencyMembershipAgreementandLiabilityRelease boolean,
 	IllinoisFoodSanitationLicense boolean,
 	HealthDepartentPermitandInspection boolean,
 	PestControl boolean
-//research how to store pdf or jpg in SQL (feature of application could be to upload a picture to form) 
+//research how to store pdf or jpg in SQL (feature of application could be to upload a picture to form)
 );
-CREATE TABLE violations_concerns ( 
+CREATE TABLE violations_concerns (
     Intrw_id int,
 	CommentsFromAgencyStaff varchar(255)
-); 
-CREATE TABLE comments_accolades ( 
-	Intrw_id int, 
+);
+CREATE TABLE comments_accolades (
+	Intrw_id int,
     CommentsFromAgencyStaff varchar(255),
     AgencyNeeds_Volunteers boolean,
     AgencyNeeds_Equiptment boolean,
     AgencyNeeds_Funding boolean,
     AgencyNeeds_Training boolean,
-    DigitalSignNIFBRep //possibly jpg of sig? 
+    DigitalSignNIFBRep //possibly jpg of sig?
     DateOfDSNIFBRep date,
     DigitalSignAgencyRep //Same^
     DataOfDSAgencyRep date
